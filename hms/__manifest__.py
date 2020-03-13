f9102a4795dd0676440bb232918800c37b1dc5df
Su Su Yi <susuyi@ZANDO.local>
2020-03-13 14:50:24 +0630

    "Changes Made"

--- hms/__manifest__.py
+++ hms/__manifest__.py
@@ -1,23 +1,28 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "hms",
-
-    'summary': """
+    'name':
+    "hms",
+    'summary':
+    """
         Short (1 phrase/line) summary of the module's purpose, used as
         subtitle on modules listing or apps.openerp.com""",
-
-    'description': """
+    'description':
+    """
         Long description of module's purpose
     """,
-
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author':
+    "My Company",
+    'website':
+    "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
+    # Check https://github.com/odoo/odoo/blob/13.0/odoo/
+    # addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.3',
+    'category':
+    'Uncategorized',
+    'version':
+    '0.3',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
