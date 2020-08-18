e3327f572ad0bbaa40abdb64a42a619abc9b51d4
unknown <thetthetaung@zandotech.com>
2020-08-18 13:32:01 +0630

    Update 18820

--- hms/__manifest__.py
+++ hms/__manifest__.py
@@ -25,7 +25,7 @@
     '1.0.0',
     # any module necessary for this one to work correctly
     'depends': [
-        'base', 'graphql_base', 'contacts', 'uom', 'account', 'mail',
+        'base', 'graphql_base', 'contacts', 'uom', 'account', 'mail','sale',
         'web', 'website', 'web_one2many_kanban','base_accounting_kit','web_tree_no_open'
     ],
     "external_dependencies": {
