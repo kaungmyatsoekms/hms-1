a5800bc8ef9f5cfcc398c895d60232ae4c912708
unknown <thetthetaung@zandotech.com>
2020-09-04 20:15:10 +0630

    Update

--- hms/__manifest__.py
+++ hms/__manifest__.py
@@ -122,6 +122,7 @@
         'wizard/hms_proforma_report_wizard_view.xml',
         'wizard/hms_split_invoice_wizard_view.xml',
         'report/proforma_portal_template.xml',
+        'views/account_invoice_view.xml'
 
         #'views/views.xml',
         # 'views/templates.xml',
